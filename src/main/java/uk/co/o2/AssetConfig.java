b17b0d130a64db118202b8a607002649191a67d0
Jitendra C <Jitendra.Chaudhary@cognizant.com>
2016-10-22 17:25:21 +0530

    [JC] #GETPUK-9 Added code to support external property file configuration

--- src/main/java/uk/co/o2/AssetConfig.java
+++ src/main/java/uk/co/o2/AssetConfig.java
@@ -4,17 +4,17 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 import uk.co.o2.log.PukLogger;
-
 @Configuration
 @EnableAspectJAutoProxy
 @ComponentScan(basePackages = {"uk.co.o2.utility"})
+@PropertySources(value = {@PropertySource(value="file://${CATALINA_BASE}/appdata/getpuk.properties",ignoreResourceNotFound=true)})
 public class AssetConfig {
-
-    //-----------------------------------------------------------------------------------------------------------------------
-    @Bean   
-    public PukLogger myLogger(){
-       return new PukLogger();
-    }
+	@Bean   
+	public PukLogger myLogger(){
+		return new PukLogger();
+	}
 }
